7aca92c2f261ade463def5ab5d603d7f31b3607a
RLCHuncp <rlch06.uncp@gmail.com>
2020-02-14 10:31:55 -0500

    cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,21 @@
 const cipher = {
   // ...
+  encode (cifrado, post) {
+    let resultado = "";
+    for (let i = 0; i < cifrado.length; i++) {
+
+      //Fórmula para posicionar la letra dentro del código ASCII
+      let codAscMen = (cifrado.charCodeAt(i) - 65 + parseInt(post)) % 26 + 65;
+      //La posicion 0 es la letra A, es decir 65 representa el código ASCII de A
+      //72
+      
+      //Concatenando letras codificadas
+      resultado = resultado + String.fromCharCode(codAscMen);
+    }
+
+    return resultado;
+
+  }
 };
 
 export default cipher;

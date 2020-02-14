e3cb06366a101815a28900e82a73eabd07fa74e9
RLCHuncp <rlch06.uncp@gmail.com>
2020-02-14 10:34:03 -0500

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import cipher from './cipher.js';
+//Variables botones
+const btnCifrar = document.getElementById("btnCifrar");
+const btnDecifrar = document.getElementById("btnDecifrar");
 
+//Evento click CIFRAR
+
+btnCifrar.addEventListener('click', () => {
+  let mensajeCifrar= document.getElementById("message1").value.toUpperCase();
+
+  let posicion = document.getElementById("offset").value;
+
+  let cifrado = cipher.encode(mensajeCifrar);
+  let post = cipher.encode(posicion);
+
+  
+
+});
 console.log(cipher);
